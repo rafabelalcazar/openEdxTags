52f216b7464349b40f5a37199cb94ea4630f8918
rafa <rafaelb@unicauca.edu.co>
2020-03-09 02:32:23 -0500

    FEAT: Capture resource tag

---
+++ reglasxblock/__init__.py
@@ -0,0 +1,1 @@
+from .reglasxblock import ReglasXBlock
---
+++ reglasxblock/static/js/src/reglasxblock.js
@@ -0,0 +1,69 @@
+/* Javascript for ReglasXBlock. */
+function ReglasXBlock(runtime, element) {
+
+
+    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+
+    $('#send', element).click(function(eventObject) {
+        var file = document.getElementById("myfile").value
+        var selector = document.getElementById("selector")
+        for(var i=0; i< selector.length;i++){
+            if (selector[i].selected===true){
+                var tag = selector[i].value
+
+                console.log('btn clicked',tag);
+            }
+
+        }
+        
+        // $.ajax({
+        //     type: "POST",
+        //     url: handlerUrl,
+        //     data: JSON.stringify({"hello": "world"}),
+        //     // success: 
+        // });
+    });
+
+    $(function ($) {
+        /* Here's where you'd do things on page load. */
+    });
+}
+
+function agregarcontenido(a){
+    tipoagregacion = a;
+    var btn = document.getElementById("send")
+    btn.removeAttribute("disabled")
+    document.getElementById("tipo_contenido").innerHTML = "";
+    if(tipoagregacion=="pdf"){
+        
+        codigo="<h3>Agregar pdf</h3>"+
+        "<form action=\'\'>"+ 
+        "<label for=\'myfile\'>Seleccionar pdf:</label>"+
+        "<input type=\'file\' id=\'myfile\' name=\'myfile\'><br><br>"+
+        "<label for='identifier'>Identificador de recurso</label>"+
+        "<input type='text' name='identifier' id='identifier' placeholder='Identificador de recurso'>"+
+        "<p>Por favor etiqueta el tipo de contenido:</p>"+
+        "<div>"+
+        "    <select name='select' id='selector' >"+
+        "        <option value='convergente' selected>Convergente</option>"+
+        "        <option value='divergente'>Divergente</option>"+
+        "        <option value='asimilador'>Asimilador</option>"+
+        "        <option value='acomodador'>Acomodador</option>"+
+        "    </select>"+
+        "</div>"+
+        "</form>";
+
+        console.log(codigo)
+        
+         document.getElementById("agregarcontenido").innerHTML = codigo;
+
+    }
+    if(tipoagregacion=="video"){
+        document.getElementById("agregarcontenido").innerHTML = "<h3>Agregar video</h3>";
+        
+
+    }
+    if(tipoagregacion=="html"){}
+    if(tipoagregacion=="discusion"){}
+    
+}
